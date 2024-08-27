2ee892b0e1716880f190e53cead10eabafe16eae
Jian Ma <jianquanma@gmail.com>
2024-08-27 16:31:16 -0400

    added task model

---
+++ AirVet/TaskModel.swift
@@ -0,0 +1,14 @@
+//
+//  TaskModel.swift
+//  AirVet
+//
+//  Created by Jian Ma on 8/27/24.
+//
+
+import Foundation
+
+struct Task {
+    let name: String
+    let description: String
+    var isCompleted: Bool
+}
